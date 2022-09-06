9b47aedd9957b32f28207479888111cd5757b05e
Hosoo <xocoo8888@gmail.com>
2022-09-06 14:51:55 +0800

    update

--- screens/Articles.js
+++ screens/Articles.js
@@ -1,10 +1,21 @@
 import { View, FlatList, Text } from "react-native";
 import s from "../utils/getRelativeSize";
 import ListItem from "../components/ListItem";
-import { articles } from "../utils/sampleData";
+// import { articles } from "../utils/sampleData";
 import RelativeTime from "../components/RelativeTime";
+import {useEffect, useState} from "react";
+import API from "../utils/API";
 
 export default function ArticlesScreen() {
+    const [articles, setArticles] = useState();
+
+    useEffect(() => {
+        API.get("a", (res) => {
+            if (res.success) {
+                setArticles(res.payload);
+            }
+        });
+    }, []);
     const renderItem = ({ item }) => <ArticleListItem item={item} />;
 
     return (
@@ -15,5 +26,5 @@ export default function ArticlesScreen() {
 }
 
 function ArticleListItem({ item }) {
-    return <ListItem title={item.title} subtitle={<RelativeTime date={item.created} />} image={{ source: item.image.source, width: s(120), height: s(67.5) }} />;
+    return <ListItem title={item.name} subtitle={<RelativeTime date={item.created} />} image={{ source: typeof item.image !== 'undefined' ? item.image.name +'_s.'+ item.image.ext : 'no-image', width: s(120), height: s(67.5) }} />;
 }
