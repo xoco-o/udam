eccda589eba94b1cdadbf088450776bdba33dc14
Hosoo <xocoo8888@gmail.com>
2022-09-08 17:43:35 +0800

    update

--- utils/recoilAtoms.js
+++ utils/recoilAtoms.js
@@ -1,4 +1,4 @@
 import { atom } from "recoil";
 
-export const userState = atom({ key: "userState", default: {} });
+export const userState = atom({ key: "userState", default: null });
 export const addSheetOpenState = atom({ key: "addSheetOpenState", default: false });
