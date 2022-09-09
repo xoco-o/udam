0adfd31a6af83538cdaeec0c4dc1e03d8e5dd369
Hosoo <xocoo8888@gmail.com>
2022-09-09 11:50:41 +0800

    гал үзэх

--- screens/OneTeam.js
+++ screens/OneTeam.js
@@ -1,22 +1,35 @@
 import { ScrollView, View } from "react-native";
 import ListItem from "../components/ListItem";
 import s from "../utils/getRelativeSize";
-import { users } from "../utils/sampleData";
+import {useEffect, useState} from "react";
+import API from "../utils/API";
 
 export default function OneTeamsScreen({ route }) {
     const { club } = route.params;
+    const [horse, setHorse] = useState();
+
+    useEffect(() => {
+        API.get("horse?club="+club.id, (res) => {
+            if (res.success) {
+                setHorse(res.payload);
+            }
+        });
+    }, []);
 
     return (
         <View style={{ flex: 1 }}>
             <ScrollView>
-                {users.map((item) => (
+                {
+                    horse ?
+                    horse.map((item) => (
                     <ListItem
                         key={item.id}
                         title={item.name}
-                        subtitle={item.description}
-                        image={{ source: item.image.source, width: s(80), height: s(80), style: { borderRadius: s(40) } }}
+                        subtitle={item.club.name}
+                        image={{ source: typeof item.image !== 'undefined' ? item.image.name +'_s.'+ item.image.ext : 'no-image', width: s(80), height: s(80), style: { borderRadius: s(40) } }}
                     />
-                ))}
+                )):<></>
+                }
             </ScrollView>
         </View>
     );
