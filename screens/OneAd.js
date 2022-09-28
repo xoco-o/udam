d79a8c5b1589dd5c4b632c6dc061cf663a644da8
Hosoo <xocoo8888@gmail.com>
2022-09-28 17:15:46 +0800

    image slide

--- screens/OneAd.js
+++ screens/OneAd.js
@@ -1,8 +1,8 @@
-import { useEffect, useState } from "react";
+import {useEffect, useRef, useState} from "react";
 import {ImageBackground, ScrollView, Text, View} from "react-native";
 import API from "../utils/API";
 import colors, {darkText} from "../utils/colors";
-import s from "../utils/getRelativeSize";
+import s, {designWidth} from "../utils/getRelativeSize";
 import urls from "../utils/urls";
 import Box from "../components/Box";
 import {BlockLoader} from "../components/Loaders";
@@ -13,6 +13,8 @@ import RelativeTime from "../components/RelativeTime";
 export default function OneAdScreen({ route }) {
     const [url, setUrl] = useState(route.params);
     const [data, setData] = useState();
+    const scrollElement = useRef(null);
+    const [active, setActive] = useState(0);
 
     useEffect(() => {
         API.get(url, (res) => {
@@ -28,13 +30,25 @@ export default function OneAdScreen({ route }) {
                 {
                     data ?
                         <View style={{margin: s(10)}}>
-                            <Box color={colors.white}>
-                                <ImageBackground
-                                    source={typeof data.images[0] !== 'undefined' ? { uri: urls.resource + data.images[0].name +'_s.'+ data.images[0].ext } : require("../assets/no-image.png")}
-                                    style={{ backgroundColor: colors.grey[300], width: '100%', height: 260, resizeMode: "cover"}}
-                                >
-                                </ImageBackground>
-                            </Box>
+                            <ScrollView
+                                ref={scrollElement}
+                                horizontal
+                                style={{width: '100%', height: 260,marginBottom: s(15)}}
+                                pagingEnabled
+                                showsHorizontalScrollIndicator={false}
+                                onMomentumScrollEnd={(event) => setActive(Math.floor(event.nativeEvent.contentOffset.x / s(designWidth-20)))}
+                            >
+                                <View style={{ flexDirection: "row" }}>
+                                    {data.images.map((item) => (
+                                        <ScrollItem item={item} key={item.id} />
+                                    ))}
+                                </View>
+                            </ScrollView>
+                            <View style={{ flexDirection: "row", position: "absolute", right: 0, left: 0, bottom: s(110), justifyContent: "center" }}>
+                                {data.images.map((_, index) => (
+                                    <PageIndicator key={index} isActive={index === active} />
+                                ))}
+                            </View>
                             <Box color={colors.white}>
                                     {
                                         typeof data.name !== 'undefined' ?
@@ -74,5 +88,19 @@ export default function OneAdScreen({ route }) {
                 {name+desc+'\n'}
             </Text>
         )
+    }
+    function ScrollItem({ item }) {
+        // console.log('-------',urls.resource + item.name +'_s.'+ item.ext)
+        return (
+                <ImageBackground
+                    source={{uri: urls.resource + item.name +'_s.'+ item.ext}}
+                    style={{width: s(designWidth-20),resizeMode: "cover"}}
+                >
+                </ImageBackground>
+        );
+    }
+
+    function PageIndicator({ isActive }) {
+        return <View style={{ width: s(10), height: s(10), backgroundColor: isActive ? "#fff" : "rgba(255,255,255,0.5)", borderRadius: s(5), marginHorizontal: s(5) }} />;
     }
 }
